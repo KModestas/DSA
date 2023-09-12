9e94cced71c6ce2e375885facbd1b92cb910cb74
Modestas <modestask97@gmail.com>
2023-09-12 11:15:04 +0100

    add queue weave example

--- queue.js
+++ queue.js
@@ -76,36 +76,8 @@ class Queue {
   }
 }
 
-function test() {
-  let myQueue = new Queue(2)
-  myQueue.enqueue(1)
-
-  // (2) Items - Returns 2 Node
-  if (myQueue.length !== 0) {
-    console.log(myQueue.dequeue().value)
-  } else {
-    console.log('undefined')
-  }
-
-  // (1) Item - Returns 1 Node
-  if (myQueue.length !== 0) {
-    console.log(myQueue.dequeue().value)
-  } else {
-    console.log('undefined')
-  }
-
-  // (0) Items - Returns undefined
-  if (myQueue.length !== 0) {
-    console.log(myQueue.dequeue().value)
-  } else {
-    console.log('undefined')
-  }
-}
-
-test()
-
-// Here is a simple implemetation of a queue using an array:
-class QueueArray {
+// *** Queue implemented using an array ***
+class Queue_Arr {
   constructor() {
     this.data = []
   }
@@ -122,3 +94,52 @@ class QueueArray {
     return this.data[this.data.length - 1]
   }
 }
+
+// Given 2 seperate queue's, add items from each queue into a new queue
+// NOTE: items have to be weaven together - 1 item from one queue followed by 1 item from the other queue
+function weave(q1, q2) {
+  const q = new Queue_Arr()
+
+  // While either q has items:
+  while (q1.peek() || q2.peek()) {
+    // add item from q1 to q
+    if (q1.peek()) {
+      q.enqueue(q1.dequeue())
+    }
+
+    // add item from q2 to q
+    if (q2.peek()) {
+      q.enqueue(q2.dequeue())
+    }
+  }
+
+  return q
+}
+
+function test() {
+  let q = new Queue(2)
+  q.enqueue(1)
+
+  // (2) Items - Returns 2 Node
+  if (q.length !== 0) {
+    console.log(q.dequeue().value)
+  } else {
+    console.log('undefined')
+  }
+
+  // (1) Item - Returns 1 Node
+  if (q.length !== 0) {
+    console.log(q.dequeue().value)
+  } else {
+    console.log('undefined')
+  }
+
+  // (0) Items - Returns undefined
+  if (q.length !== 0) {
+    console.log(q.dequeue().value)
+  } else {
+    console.log('undefined')
+  }
+}
+
+test()
