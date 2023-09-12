d57056c2911f3c92ef1e32ea3d05ecbbabf1c568
Modestas <modestask97@gmail.com>
2023-09-12 11:13:55 +0100

    add stack as array example

--- stack.js
+++ stack.js
@@ -64,6 +64,67 @@ class Stack {
   }
 }
 
+// *** Stack implemented using an array ***
+class Stack_Arr {
+  constructor() {
+    this.stackList = []
+  }
+
+  getStackList() {
+    return this.stackList
+  }
+
+  printStack() {
+    for (let i = this.stackList.length - 1; i >= 0; i--) {
+      console.log(this.stackList[i])
+    }
+  }
+
+  isEmpty() {
+    return this.stackList.length === 0
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null
+    } else {
+      return this.stackList[this.stackList.length - 1]
+    }
+  }
+
+  size() {
+    return this.stackList.length
+  }
+
+  push(value) {
+    this.stackList.push(value)
+  }
+
+  pop() {
+    if (this.isEmpty()) return null
+    return this.stackList.pop()
+  }
+}
+
+// Use stack to reverse a string:
+function reverseString(str) {
+  const stack = new Stack_Arr() // Create a new stack instance
+  let reversedString = '' // Initialize an empty reversed string
+
+  // Iterate through each character in the input string
+  for (const c of str) {
+    stack.push(c) // Push each character onto the stack
+  }
+
+  // Continue until the stack is empty
+  while (!stack.isEmpty()) {
+    // Pop characters from the stack and append them to the reversed string
+    reversedString += stack.pop()
+  }
+
+  return reversedString // Return the reversed string
+}
+
 function test() {
   let myStack = new Stack(2)
   myStack.push(1)
